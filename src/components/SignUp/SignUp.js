44c816208cec8759715250a39d7d91206ecf0b57
ratulinspace <i.shahriarislam.ratul@gmail.com>
2022-04-19 23:09:15 +0600

    UI color and theam changed

---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            <h2>SIGH UP</h2>
+        </div>
+    );
+};
+
+export default SignUp;
--- src/components/hooks/useProducts.js
+++ src/components/hooks/useProducts.js
@@ -2,19 +2,15 @@ import { useEffect, useState } from "react"
 
 const useProducts = () => {
 
-
     const [products, setProducts] = useState([]);
-
 
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
-            .then(data => setProducts(data))
+            .then(data => setProducts(data));
     }, []);
 
-
     return [products, setProducts];
-};
-
+}
 
 export default useProducts;
