40847f0e4de3661f01fe66ff3d8872abfc66349a
ratulinspace <i.shahriarislam.ratul@gmail.com>
2022-04-21 21:20:39 +0600

    implemented login and redrect to shop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,36 @@
 import React, { useState } from 'react';
+import menuIcon from '../../images/menu-icon.png'
 import { Link } from 'react-router-dom';
 import logo from "../../images/Logo.svg"
 import './Header.css'
 
 const Header = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
     return (
-        <nav className='header'>
-            <Link to={"/"}><img src={logo} alt="" /></Link>
-            <div className='nav-items'>
-                <Link to={'/shop'}>Shop</Link>
-                <Link to={'/orders'}>Orders</Link>
-                <Link to={'/inventory'}>Inventory</Link>
-                <Link to={'/about'}>About</Link>
-                <Link to={'/login'}>Login</Link>
-                <Link to={'/signup'}>SignUp</Link>
-            </div>
-        </nav>
+        <div>
+            <nav className='header'>
+                <Link to={"/"}><img id='logo' src={logo} alt="logo" /></Link>
+                {toggleMenu && <div className='nav-items-sm'>
+                    <Link to={'/shop'}>Shop</Link>
+                    <Link to={'/orders'}>Orders</Link>
+                    <Link to={'/inventory'}>Inventory</Link>
+                    <Link to={'/about'}>About</Link>
+                    <Link to={'/login'}>Login</Link>
+                    <Link to={'/signup'}>SignUp</Link>
+                </div>}
+                <div className='nav-items-lg'>
+                    <Link to={'/shop'}>Shop</Link>
+                    <Link to={'/orders'}>Orders</Link>
+                    <Link to={'/inventory'}>Inventory</Link>
+                    <Link to={'/about'}>About</Link>
+                    <Link to={'/login'}>Login</Link>
+                    <Link to={'/signup'}>SignUp</Link>
+                </div>
+                <div id="menu-icon" onClick={() => setToggleMenu(!toggleMenu)}>
+                    <img height="40px" width="40px" src={menuIcon} alt="menu bar" />
+                </div>
+            </nav>
+        </div>
     );
 };
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from "../../firebase.init";
 import "./Login.css";
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+    if (user) {
+        navigate("/shop");
+    }
+    const handleUserSignIn = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    }
+
     return (
         <div className='form-container p-3'>
             <div>
                 <h2 className='form-title mt-4'>Login</h2>
-                <form action="">
+                <form action="" onSubmit={handleUserSignIn}>
                     <div className="input-group">
                         <label htmlFor="Email">Email</label>
-                        <input type="email" name="" id="" required />
+                        <input onBlur={handleEmailBlur} type="email" name="" id="" required />
                     </div>
                     <div className='input-group'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" required />
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
                     </div>
+                    <p className='text-red-600'>{error?.message}</p>
+                    {
+                        loading && <p>Loading...</p>
+                    }
+                    <br />
                     <input className='form-submit' type="submit" value="Login" />
                 </form>
                 <p>
